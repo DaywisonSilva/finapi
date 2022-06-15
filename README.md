47f617d2e331f3c25e19f510aeeaaec070f1a8bc
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-06-14 21:32:47 -0300

    feat: to handle with middleware

--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 
 :white_check_mark: Não deve ser possível cadastrar uma conta com CPF já existente
 
-[] Não deve ser possível fazer depósito em uma conta não existente
+:white_check_mark: Não deve ser possível fazer depósito em uma conta não existente
 
 [] Não deve ser possível buscar extrato em uma conta não existente
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+const { request } = require('express')
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
-
 const app = express()
 
 const customers = []
 
 app.listen(3000, () => console.log('listen on port 3000'))
-
 app.use(express.json())
 
-app.get('/', (_, res) => {
-  res.json({ message: 'works fine' })
-})
+const verifyIfExistAccountCPF = (req, res, next) => {
+  const { cpf } = req.params
+  const customer = customers.find((customer) => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).send({ error: 'customer not found' })
+  }
+  req.customer = customer
+  return next()
+}
 
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body
@@ -30,4 +36,9 @@ app.post('/account', (req, res) => {
   customers.push({ cpf, name, id, statement: [] })
 
   return res.status(201).send()
+})
+
+app.get('/statement/:cpf', verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req
+  return res.json(customer.statement)
 })
